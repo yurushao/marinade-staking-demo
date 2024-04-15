9943db7ef47bffc6dbac7a4eef2102bf5b90a666
yurushao <shaoyuru@gmail.com>
2024-04-14 21:43:51 -0700

    Add marinade cpi using anchor-gen

--- programs/marinade-staking-demo/src/lib.rs
+++ programs/marinade-staking-demo/src/lib.rs
@@ -1,15 +1,41 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{transfer, Mint, Token, TokenAccount, Transfer},
+};
+use marinade::cpi::accounts::{Deposit as MarinadeDeposit, LiquidUnstake as MarinadeLiquidUnstake};
+use marinade::cpi::{deposit as marinade_deposit, liquid_unstake as marinade_liquid_unstake};
+use marinade::program::MarinadeFinance;
+use marinade::State as MarinadeState;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("DfHJpPQ7BNhGz9LNvohUgqFMDRqfLDeDXk8GKqnryNPT");
 
 #[program]
 pub mod marinade_staking_demo {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn deposit<'c: 'info, 'info>(_ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        msg!("Depositing {} tokens", amount);
+        Ok(())
+    }
+
+    pub fn unstake<'c: 'info, 'info>(_ctx: Context<Unstake>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub marinade_program: Program<'info, MarinadeFinance>,
+    // pub drift_program: Program<'info, Drift>,
+}
+
+#[derive(Accounts)]
+pub struct Unstake<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    // pub marinade_program: Program<'info, MarinadeFinance>,
+}
--- tests/marinade-staking-demo.ts
+++ tests/marinade-staking-demo.ts
@@ -1,16 +1,48 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import {
+  Program,
+  AnchorProvider,
+  setProvider,
+  workspace,
+  BN,
+} from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
 import { MarinadeStakingDemo } from "../target/types/marinade_staking_demo";
 
 describe("marinade-staking-demo", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = AnchorProvider.env();
+  setProvider(provider);
+  const payer = provider.wallet;
+  const connection = provider.connection;
 
-  const program = anchor.workspace.MarinadeStakingDemo as Program<MarinadeStakingDemo>;
+  console.log("Payer address", payer.publicKey.toBase58());
+
+  const marinadeProgram = new PublicKey(
+    "MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD"
+  );
+
+  const driftProgram = new PublicKey(
+    "dRiftyHA39MWEi3m9aunc5MzRF1JYuBsbn6VPcn33UH"
+  );
+
+  const program = workspace.MarinadeStakingDemo as Program<MarinadeStakingDemo>;
+
+  console.log("Program ID", program.programId.toBase58());
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    try {
+      const tx = await program.methods
+        .deposit(new BN(1e9))
+        .accounts({
+          signer: payer.publicKey,
+          marinadeProgram,
+        })
+        .rpc({ commitment: "confirmed" });
+      console.log("Your transaction signature", tx);
+    } catch (error) {
+      console.log("Error", error);
+      throw error;
+    }
   });
 });
