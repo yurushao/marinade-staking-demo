afa3dd18ca39592b756221f529fbadd480612165
yurushao <shaoyuru@gmail.com>
2024-04-16 21:25:55 -0700

    Stake with a wallet address

--- programs/marinade-staking-demo/src/lib.rs
+++ programs/marinade-staking-demo/src/lib.rs
@@ -14,8 +14,23 @@ declare_id!("DfHJpPQ7BNhGz9LNvohUgqFMDRqfLDeDXk8GKqnryNPT");
 pub mod marinade_staking_demo {
     use super::*;
 
-    pub fn deposit<'c: 'info, 'info>(_ctx: Context<Deposit>, amount: u64) -> Result<()> {
-        msg!("Depositing {} tokens", amount);
+    pub fn deposit<'c: 'info, 'info>(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        let cpi_program = ctx.accounts.marinade_program.to_account_info();
+        let cpi_accounts = MarinadeDeposit {
+            state: ctx.accounts.marinade_state.to_account_info(),
+            msol_mint: ctx.accounts.msol_mint.to_account_info(),
+            liq_pool_sol_leg_pda: ctx.accounts.liq_pool_sol_leg_pda.to_account_info(),
+            liq_pool_msol_leg: ctx.accounts.liq_pool_msol_leg.to_account_info(),
+            liq_pool_msol_leg_authority: ctx.accounts.liq_pool_msol_leg_authority.to_account_info(),
+            reserve_pda: ctx.accounts.reserve_pda.to_account_info(),
+            transfer_from: ctx.accounts.signer.to_account_info(),
+            mint_to: ctx.accounts.mint_to.to_account_info(),
+            msol_mint_authority: ctx.accounts.msol_mint_authority.to_account_info(),
+            system_program: ctx.accounts.system_program.to_account_info(),
+            token_program: ctx.accounts.token_program.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        let _ = marinade_deposit(cpi_ctx, amount);
         Ok(())
     }
 
@@ -29,13 +44,51 @@ pub struct Deposit<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
 
+    #[account(mut)]
+    pub marinade_state: Account<'info, MarinadeState>,
+
+    /// CHECK: skip
+    #[account(mut)]
+    pub reserve_pda: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub msol_mint: Account<'info, Mint>,
+
+    /// CHECK: skip
+    #[account(mut)]
+    pub msol_mint_authority: AccountInfo<'info>,
+
+    /// CHECK: skip
+    #[account(mut)]
+    pub liq_pool_msol_leg: AccountInfo<'info>,
+
+    /// CHECK: skip
+    #[account(mut)]
+    pub liq_pool_msol_leg_authority: AccountInfo<'info>,
+
+    /// CHECK: skip
+    #[account(mut)]
+    pub liq_pool_sol_leg_pda: AccountInfo<'info>,
+
+    // #[account(
+    //     init_if_needed,
+    //     payer = signer,
+    //     associated_token::mint = share_class,
+    //     associated_token::authority = signer,
+    //     associated_token::token_program = token_2022_program
+    // )]
+    #[account(mut)]
+    pub mint_to: Account<'info, TokenAccount>,
+
     pub marinade_program: Program<'info, MarinadeFinance>,
-    // pub drift_program: Program<'info, Drift>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
 }
 
 #[derive(Accounts)]
 pub struct Unstake<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
-    // pub marinade_program: Program<'info, MarinadeFinance>,
+    pub marinade_program: Program<'info, MarinadeFinance>,
 }
--- tests/marinade-staking-demo.ts
+++ tests/marinade-staking-demo.ts
@@ -7,35 +7,88 @@ import {
 } from "@coral-xyz/anchor";
 import { PublicKey } from "@solana/web3.js";
 import { MarinadeStakingDemo } from "../target/types/marinade_staking_demo";
+import { Marinade, MarinadeConfig } from "@marinade.finance/marinade-ts-sdk";
+import { getAssociatedTokenAddressSync } from "@solana/spl-token";
 
 describe("marinade-staking-demo", () => {
   // Configure the client to use the local cluster.
   const provider = AnchorProvider.env();
   setProvider(provider);
-  const payer = provider.wallet;
+  const wallet = provider.wallet;
   const connection = provider.connection;
 
-  console.log("Payer address", payer.publicKey.toBase58());
+  const config = new MarinadeConfig({
+    connection: connection,
+    publicKey: wallet.publicKey,
+  });
+  const marinade = new Marinade(config);
+  let marinadeState;
+
+  before(async () => {
+    /*
+    try {
+      const { transaction: liqTx } = await marinade.addLiquidity(
+        MarinadeUtils.solToLamports(100)
+      );
+      await provider.sendAndConfirm(liqTx);
+    } catch (err) {
+      console.error("Failure on beforeAll addLiquidity transaction", err);
+      throw err;
+    }
+    */
+    marinadeState = await marinade.getMarinadeState();
+
+    console.log(
+      "marinadaState reservePda",
+      await marinadeState.reserveAddress()
+    );
+    console.log("marinadaState state", marinadeState.marinadeStateAddress);
+    console.log("marinadaState msolMint", marinadeState.mSolMintAddress);
+    console.log(
+      "marinadaState msolMintAuthority",
+      await marinadeState.mSolMintAuthority()
+    );
+    console.log(
+      "marinadaState liqPoolMsolLegAuthority",
+      await marinadeState.mSolLegAuthority()
+    );
+    console.log("marinadaState liqPoolMsolLeg", marinadeState.mSolLeg);
+    console.log("marinadaState liqPoolSolLegPda", await marinadeState.solLeg());
+  });
 
   const marinadeProgram = new PublicKey(
     "MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD"
   );
 
-  const driftProgram = new PublicKey(
-    "dRiftyHA39MWEi3m9aunc5MzRF1JYuBsbn6VPcn33UH"
-  );
-
   const program = workspace.MarinadeStakingDemo as Program<MarinadeStakingDemo>;
 
-  console.log("Program ID", program.programId.toBase58());
-
   it("Is initialized!", async () => {
+    const { associatedMSolTokenAccountAddress, transaction } =
+      await marinade.deposit(new BN(1));
+    const signature = await provider.sendAndConfirm(transaction);
+    console.log(
+      "associatedMSolTokenAccountAddress",
+      associatedMSolTokenAccountAddress
+    );
+
+    // create treasury account
+    // create treasury ata for msol
+    // getAssociatedTokenAddressSync(marinadeState.mSolMintAddress);
+
     // Add your test here.
     try {
       const tx = await program.methods
-        .deposit(new BN(1e9))
+        .deposit(new BN(1e10))
         .accounts({
-          signer: payer.publicKey,
+          signer: wallet.publicKey,
+          reservePda: await marinadeState.reserveAddress(),
+          marinadeState: marinadeState.marinadeStateAddress,
+          msolMint: marinadeState.mSolMintAddress,
+          msolMintAuthority: await marinadeState.mSolMintAuthority(),
+          liqPoolMsolLeg: marinadeState.mSolLeg,
+          liqPoolMsolLegAuthority: await marinadeState.mSolLegAuthority(),
+          liqPoolSolLegPda: await marinadeState.solLeg(),
+          mintTo: associatedMSolTokenAccountAddress,
           marinadeProgram,
         })
         .rpc({ commitment: "confirmed" });
